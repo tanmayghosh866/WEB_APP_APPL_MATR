c91e190421415794c00b9fb82975fc85c55e72a8
tanmayghosh866 <89400265+tanmayghosh866@users.noreply.github.com>
2023-06-25 13:52:20 +0530

    Month Grouping added to app Q1.py
    Added Month grouping by names and top 3 months for Gold and silver prices

--- Q1.py
+++ Q1.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 import warnings
 warnings.filterwarnings('ignore')
 data = pd.read_csv('EDA_Gold_Silver_prices.csv')
@@ -43,8 +44,10 @@ st.set_option('deprecation.showPyplotGlobalUse', False)
 st.header('Data Filtering')
 
 # Filtering options
-price_range = st.slider('Select Price Range', min_value=float(data['GoldPrice'].min()),
-                        max_value=float(data['GoldPrice'].max()), value=(float(data['GoldPrice'].min()), float(data['GoldPrice'].max())))
+price_range = st.slider('Select Price Range', 
+                        min_value=float(data['GoldPrice'].min()),
+                        max_value=float(data['GoldPrice'].max()), 
+                        value=(float(data['GoldPrice'].min()), float(data['GoldPrice'].max())))
 
 filtered_data = data[(data['GoldPrice'].between(price_range[0], price_range[1]))]
 
@@ -54,9 +57,65 @@ st.write(filtered_data)
 
 if st.button('Group by Month'):
     data['Month'] = pd.to_datetime(data['Month'], errors='coerce').dt.month  # Extract month from date
-    grouped_data = data.groupby('Month').mean()
+    grouped_data = data.groupby('Month')['GoldPrice', 'SilverPrice'].mean().round(0).reset_index()
+    grouped_data['Month'] = grouped_data['Month'].round(0).astype(str)
+    data['Month'] = pd.to_datetime(data['Month'], errors='coerce').dt.month  # Extract month from date
+    Month_Name = []
+    for i in range(grouped_data.shape[0]):
+        month_num = grouped_data['Month'][i]
+        if month_num == '1.0':
+            res = 'January'
+        elif month_num == '2.0':
+            res = 'February'
+        elif month_num == '3.0':
+            res = 'March'
+        elif month_num == '4.0':
+            res = 'April'
+        elif month_num == '5.0':
+            res = 'May'
+        elif month_num == '6.0':
+            res = 'June'
+        elif month_num == '7.0':
+            res = 'July'
+        elif month_num == '8.0':
+            res = 'August'
+        elif month_num == '9.0':
+            res = 'September'
+        elif month_num == '10.0':
+            res = 'October'
+        elif month_num == '11.0':
+            res = 'November'
+        elif month_num == '12.0':
+            res = 'December'
+        else:
+            res = 'Unknown'
+        Month_Name.append(res)
+
+    grouped_data['Month_Name'] = Month_Name
+    # Top 5 brands by profit
+    top_5_month_gold = grouped_data.nlargest(5, 'GoldPrice')
+    plt.figure(figsize=(10, 6))
+    sns.barplot(data=top_5_month_gold, x='Month_Name', y='GoldPrice')
+    plt.title('Top 5 Months of Gold Price')
+    plt.xlabel('Month')
+    plt.ylabel('Gold Price')
+    st.pyplot()
+
+    # Bottom 5 brands by profit
+    top_5_month_silver = grouped_data.nsmallest(5, 'SilverPrice')
+    plt.figure(figsize=(10, 6))
+    sns.barplot(data=top_5_month_silver, x='Month_Name', y='SilverPrice')
+    plt.title('Top 5 Months Silver Price')
+    plt.xlabel('Month')
+    plt.ylabel('Silver Price')
+    st.pyplot()
+
+    grouped_data['SilverPrice'] = grouped_data['SilverPrice'].astype(str) + ' INR'
+    grouped_data['GoldPrice'] = grouped_data['GoldPrice'].astype(str) + ' INR'
     st.subheader('Grouped Data by Month - prices in each month across years averages')
     st.write(grouped_data)
+    
+    
     
 if st.button('Correlation Matrix'):
         data['Month'] = pd.to_datetime(data['Month'], errors='coerce').dt.month
